0324e3ca47c1954b38cdfc654130f5eec08ea28f
Jason Gong <mhpjay@gmail.com>
2020-07-13 22:24:53 -0400

    add 215Kth Largest Element in an Array

---
+++ 215KthLargest.js
@@ -0,0 +1,16 @@
+// 215. Kth Largest Element in an Array
+
+// Example 1:
+
+//     Input: [3, 2, 1, 5, 6, 4] and k = 2
+// Output: 5
+// Example 2:
+
+//     Input: [3, 2, 3, 1, 2, 4, 5, 5, 6] and k = 4
+// Output: 4
+// Note:
+//     You may assume k is always valid, 1≤ k≤ array 's length.
+
+var findKthLargest = function (nums, k) {
+
+};
